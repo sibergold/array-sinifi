6b04d1fc0c03d70e636957804ebf33f8738323f4
sibergold <kaynakca21@hotmail.com>
2022-08-19 00:47:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace arrays{
+    class Program{
+        private static void Main(string[] args){
+            int[] sayiDizisi={23,12,4,86,72,3,11,17};
+        Console.WriteLine("**** Sırasız Dizi ****");
+        foreach(var sayi in sayiDizisi)
+        Console.WriteLine(sayi);    
+        Console.WriteLine("**** Sıralı Dizi *****");
+        Array.Sort(sayiDizisi);
+        foreach(var sayi in sayiDizisi)
+        Console.WriteLine(sayi);
+       Console.WriteLine("**** Array Clear ****");
+       Array.Clear(sayiDizisi,2,2);
+       foreach(var sayi in sayiDizisi)
+       Console.WriteLine(sayi);
+       Console.WriteLine("**** Array Reverse ****");
+       Array.Reverse(sayiDizisi);
+       foreach(var sayi in sayiDizisi)
+       Console.WriteLine(sayi);
+       Console.WriteLine("**** Array İndexOf ****");
+       Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+       Console.WriteLine("**** Array Resize ****");
+       Array.Resize<int>(ref sayiDizisi,9);
+       sayiDizisi[8]=99;
+       foreach(var sayi in sayiDizisi)
+       Console.WriteLine(sayi);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
